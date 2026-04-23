3e533891909fb03a60229ffa74ff17aff4a23e19
iankisali <iankisali@gmail.com>
2026-04-23 21:45:23 +0300

    provision sagemeker serverless endpoint from huggingface

---
+++ terraform/1_sagemaker/main.tf
@@ -0,0 +1,90 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.70"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+# Data source for current caller identity
+data "aws_caller_identity" "current" {}
+
+# IAM Role to be used by sagemaker
+resource "aws_iam_role" "sagemaker_role" {
+  name = "agentra-sagemaker-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "sagemaker.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sagemaker_full_access" {
+  role       = aws_iam_role.sagemaker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
+
+# Define Sagemaker Model from HuggingFace
+resource "aws_sagemaker_model" "embedding_model" {
+  name               = "agentra-embedding-model"
+  execution_role_arn = aws_iam_role.sagemaker_role.arn
+
+  primary_container {
+    image = var.sagemaker_image_uri
+    environment = {
+      HF_MODEL_ID = var.embedding_model_name
+      HF_TASK     = "feature-extraction"
+    }
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.sagemaker_full_access]
+}
+
+# Serverless Inference configuration
+resource "aws_sagemaker_endpoint_configuration" "serverless_config" {
+  name = "agentra-embedding-serverless-config"
+
+  production_variants {
+    model_name = aws_sagemaker_model.embedding_model.name
+    
+    serverless_config {
+      memory_size_in_mb = 3072
+      max_concurrency   = 2
+    }
+  }
+}
+
+resource "time_sleep" "wait_for_iam_propagation" {
+  depends_on = [
+    aws_iam_role_policy_attachment.sagemaker_full_access
+  ]
+  
+  create_duration = "15s"
+}
+
+# SageMaker Endpoint
+resource "aws_sagemaker_endpoint" "embedding_endpoint" {
+  name                 = "agentra-embedding-endpoint"
+  endpoint_config_name = aws_sagemaker_endpoint_configuration.serverless_config.name
+  
+  depends_on = [
+    time_sleep.wait_for_iam_propagation
+  ]
+  
+}
